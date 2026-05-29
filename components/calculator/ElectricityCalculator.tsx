827a8402af0bfc9d3b0248f639263230bf350f3f
mmspmohammed <mohyoss6655@gmail.com>
2026-05-29 22:38:25 +0300

    Create ElectricityCalculator.tsx

---
+++ components/calculator/ElectricityCalculator.tsx
@@ -0,0 +1,11 @@
+// components/calculator/ElectricityCalculator.tsx
+
+"use client";
+
+export default function ElectricityCalculator() {
+  return (
+    <div className="rounded-2xl bg-card p-4">
+      Electricity Calculator
+    </div>
+  );
+}
