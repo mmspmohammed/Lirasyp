6269980f74d395bdc702f13af8516eda073b80da
mmspmohammed <mohyoss6655@gmail.com>
2026-05-29 22:36:47 +0300

    Create GoldCalculator.tsx

---
+++ components/calculator/GoldCalculator.tsx
@@ -0,0 +1,11 @@
+// components/calculator/GoldCalculator.tsx
+
+"use client";
+
+export default function GoldCalculator() {
+  return (
+    <div className="rounded-2xl bg-card p-4">
+      Gold Calculator
+    </div>
+  );
+}
