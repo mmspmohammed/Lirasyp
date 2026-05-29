bd8bf7e619dfda02ce32c154bdf006a2564c64a2
mmspmohammed <mohyoss6655@gmail.com>
2026-05-29 21:36:47 +0300

    Create env.ts

---
+++ lib/env.ts
@@ -0,0 +1,6 @@
+// lib/env.ts
+export const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME || 'ليرا سورية';
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://lirasyp.sy';
+export const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+export const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+export const NEXT_PUBLIC_VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || '';
