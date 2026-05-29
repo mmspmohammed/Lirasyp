63f22baa0359a592a33592f91678e963ea21c72d
mmspmohammed <mohyoss6655@gmail.com>
2026-05-29 22:39:31 +0300

    Create FuelCalculator.tsx

---
+++ components/calculator/FuelCalculator.tsx
@@ -0,0 +1,11 @@
+// components/calculator/FuelCalculator.tsx
+
+"use client";
+
+export default function FuelCalculator() {
+  return (
+    <div className="rounded-2xl bg-card p-4">
+      Fuel Calculator
+    </div>
+  );
+}
