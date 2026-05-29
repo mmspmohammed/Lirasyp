4e96e6b889e9221e6a39e369722cfccad7a7e83e
mmspmohammed <mohyoss6655@gmail.com>
2026-05-29 22:36:07 +0300

    Create CryptoCalculator.tsx

---
+++ components/calculator/CryptoCalculator.tsx
@@ -0,0 +1,11 @@
+// components/calculator/CryptoCalculator.tsx
+
+"use client";
+
+export default function CryptoCalculator() {
+  return (
+    <div className="rounded-2xl bg-card p-4">
+      Crypto Calculator
+    </div>
+  );
+}
