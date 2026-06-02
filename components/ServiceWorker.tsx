be885ae19f79ff1204d60f7024578486d327afcd
mmspmohammed <mohyoss6655@gmail.com>
2026-06-02 13:11:53 +0300

    Update ServiceWorker.tsx

--- components/ServiceWorker.tsx
+++ components/ServiceWorker.tsx
@@ -3,7 +3,7 @@
 
 import { useEffect } from "react";
 
-export default function ServiceWorkerRegistration() {
+export default function SwRegistration() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
